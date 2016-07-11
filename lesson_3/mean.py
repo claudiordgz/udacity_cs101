8958ad4ea9848cecd602d1c99c129b6d002be03d
claudiordgz <claudio.rdgz@gmail.com>
2016-07-10 20:41:00 -0400

    lesson 3 mean

---
+++ lesson_3/mean.py
@@ -0,0 +1,17 @@
+def list_mean(l):
+    try:
+        a = sum(l)
+        return float(a)/len(l)
+    except:
+        return 0.0
+
+print list_mean([1,2,3,4])
+#>>> 2.5
+print list_mean([1,3,4,5,2])
+#>>> 3.0
+print list_mean([])
+#>>> ??? You decide. If you decide it should give an error, comment
+# out the print line above to prevent your code from being graded as
+# incorrect.
+print list_mean([2])
+#>>> 2.0

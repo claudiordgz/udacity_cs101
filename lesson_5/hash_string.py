7c6fa5315b0864a975dd9ae33d671157ca1bbcf4
claudiordgz <claudio.rdgz@gmail.com>
2016-07-16 23:01:00 -0400

    lesson 5 hash_string.py

---
+++ lesson_5/hash_string.py
@@ -0,0 +1,23 @@
+# Define a function, hash_string,
+# that takes as inputs a keyword
+# (string) and a number of buckets,
+# and returns a number representing
+# the bucket for that keyword.
+
+hash_string = lambda keyword,buckets: sum(map(lambda x: ord(x), keyword)) % buckets
+
+
+print hash_string('a',12)
+#>>> 1
+
+print hash_string('b',12)
+#>>> 2
+
+print hash_string('a',13)
+#>>> 6
+
+print hash_string('au',12)
+#>>> 10
+
+print hash_string('udacity',12)
+#>>> 11
